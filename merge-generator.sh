225457e317d0926b58b416d552e9f4e411e81e6d
Rin Minase <rin.black.raison.detre@gmail.com>
2021-08-01 17:13:25 +0800

    Fix subs parsing, included information messages

--- merge-generator.sh
+++ merge-generator.sh
@@ -6,20 +6,40 @@ declare -a audio
 declare -a subs
 rm -f merge.bat
 
+# color codes
+sh_b="\e[1;31m" # blue, welcome text
+sh_g="\e[1;32m" # green, success text
+sh_c="\e[1;36m" # cyan, info text
+sh_r="\e[0m" # reset color
+
+echo -e "${sh_b} Loading merge script generator ${sh_r}"
+
 # loop over all MP4 files
+echo -e "${sh_c} INFO ${sh_r} Looking for all mkv files present in the root folder"
 for mkv in *.mkv ; do
   base="${mkv%mkv}"
   args=(-o "\"output/${base}mkv\"" "\"${mkv}\"")
+  subs=()
+  audio=()
 
   # look for subtitles with the same base name
-  if [[ -f "${base}ass" ]]; then
-    subs=("\"${base}ass\"")
-  fi
+  echo -e "${sh_c} INFO ${sh_r} Looking for subtitles with the same base name"
+  for subsFile in *.ass ; do
+    subsFileBase=$(grep -F "${base}" <<<"$subsFile")
+
+    if [ -e "${subsFileBase}" ]; then
+      subs=("${subs[@]}" "\"${subsFileBase}\"")
+      break
+    fi
+  done
   
   # look for eng folder
+  echo -e "${sh_c} INFO ${sh_r} Checking if 'eng' folder is present"
   if [[ -d "eng" ]]; then
+    echo -e "${sh_c} INFO ${sh_r} 'eng' folder is present"
 
     # look for eng audio with same base name
+    echo -e "${sh_c} INFO ${sh_r} Looking for audio with the same base name"
     for engfile in eng/*.mka ; do
       engFileBase=$(grep -F "${base}" <<<"$engfile")
 
@@ -30,6 +50,7 @@ for mkv in *.mkv ; do
     done
 
     # look for eng subtitles with same base name
+    echo -e "${sh_c} INFO ${sh_r} Looking for other subtitles with the same base name in 'eng' folder"
     for engfile in eng/*.ass ; do
       engFileBase=$(grep -F "${base}" <<<"$engfile")
 
@@ -43,6 +64,7 @@ for mkv in *.mkv ; do
   args=("${args[@]}" "${audio[@]}" "${subs[@]}")
   
   # look for fonts folder
+  echo -e "${sh_c} INFO ${sh_r} Looking for 'fonts' folder"
   if [[ -d "fonts" ]]; then
     for font in fonts/*.ttf ; do
       args=("${args[@]}" "--attachment-mime-type application/x-truetype-font --attach-file \"${font}\"")
@@ -54,5 +76,9 @@ for mkv in *.mkv ; do
   fi
 
   # create output file
-  echo mkvmerge "${args[@]}" >> merge.bat
+  echo -e "${sh_c} INFO ${sh_r} Appending to the output file"
+  echo -e mkvmerge "${args[@]}" >> merge.bat
 done
+
+echo -e "${sh_g} SUCCESS ${sh_r} Finished creating output file"
+sleep 2
